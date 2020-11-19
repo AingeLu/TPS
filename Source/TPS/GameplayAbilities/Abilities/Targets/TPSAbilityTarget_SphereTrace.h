e0c7cec1b5e3a375362c49daf42486eb8c2fa7b1
lusong <13617338374@163.com>
2020-11-19 22:14:57 +0800

    add

---
+++ Source/TPS/GameplayAbilities/Abilities/Targets/TPSAbilityTarget_SphereTrace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/Abilities/Targets/TPSAbilityTarget.h"
+#include "TPSAbilityTarget_SphereTrace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_API UTPSAbilityTarget_SphereTrace : public UTPSAbilityTarget
+{
+	GENERATED_BODY()
+	
+};
